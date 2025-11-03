995073ab6cdc0cbaf0a98ea4b9eed623164623f8
davemasino <davemasino@users.noreply.github>
2025-11-02 21:28:45 -0600

    Switch dev environment to C tooling

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "C++ Development with Nix";
+  description = "C Development with Nix";
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
@@ -8,29 +8,19 @@
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        # To import an internal flake module: ./other.nix
-        # To import an external flake module:
-        #   1. Add foo to inputs
-        #   2. Add foo as a parameter to the outputs function
-        #   3. Add here: foo.flakeModule
-
-      ];
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
         # Per-system attributes can be defined here. The self' and inputs'
         # module parameters provide easy access to attributes of the same
         # system.
 
-        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-        packages.default = pkgs.hello;
-
         devShells.default = pkgs.mkShell {
             packages = with pkgs; [
-              # C++ Compiler is already part of stdenv
-              boost
-              catch2
-              cmake
+              # C compiler is already part of stdenv; Meson/Ninja drive the build
+              cmocka
+              meson
+              ninja
+              pkg-config
               starship
             ];
             shellHook = ''
@@ -43,11 +33,5 @@
           };
 
         };
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
-
-      };
     };
 }
